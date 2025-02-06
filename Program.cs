f3edf5af1799d7f3fb35ccc63ce68c68d9ddab5f
yael <yaelina5261@gmail.com>
2025-02-06 21:39:39 +0200

    domain

--- Program.cs
+++ Program.cs
@@ -12,16 +12,29 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 // הוספת Cors
+// builder.Services.AddCors(options =>
+// {
+//     options.AddPolicy("AllowSpecific",
+//                       policy  =>
+//                       {
+//                           policy.AllowAnyOrigin()
+//                                  .AllowAnyHeader() 
+//                                  .AllowAnyMethod();   // מאפשר כל שיטת HTTP
+//                       });
+// });
+
+// הוספת Cors
 builder.Services.AddCors(options =>
 {
-    options.AddPolicy("AllowSpecific",
-                      policy  =>
-                      {
-                          policy.AllowAnyOrigin()
-                                 .AllowAnyHeader() 
-                                 .AllowAnyMethod();   // מאפשר כל שיטת HTTP
-                      });
+    options.AddPolicy("AllowSpecific", policy =>
+ {
+     policy.WithOrigins("https://todolistclient-19b1.onrender.com")
+           .AllowAnyHeader()
+           .AllowAnyMethod();
+ });
 });
+
+
 
 // הוספת שירותי חיבור למסד נתונים
 builder.Services.AddScoped<IDbConnection>(sp =>
@@ -56,20 +69,20 @@ app.MapGet("/items", async (IDbConnection db) =>
 app.MapPost("/items", async ([FromBody] ItemDTOPost itemDTOPost, IDbConnection db) =>
 {
     var query = "insert into items (Name,IsComplete) values (@Name,false) ";
-    var result = await db.ExecuteAsync(query, new{Name=itemDTOPost.Name});
+    var result = await db.ExecuteAsync(query, new { Name = itemDTOPost.Name });
 
     return Results.Ok(new { Message = "successFul Post" });
 });
 
 app.MapPut("/items/{id}", async (int id, [FromBody] ItemDTOPut itemDTOPut, IDbConnection db) =>
 {
-    if (id<=0)
+    if (id <= 0)
     {
         return Results.BadRequest(new { Message = "ID is required" });
     }
 
     var query = "UPDATE items SET IsComplete = @IsComplete WHERE Id = @Id";
-    var result = await db.ExecuteAsync(query, new { Id = id,itemDTOPut.IsComplete });
+    var result = await db.ExecuteAsync(query, new { Id = id, itemDTOPut.IsComplete });
 
     if (result > 0)
     {
